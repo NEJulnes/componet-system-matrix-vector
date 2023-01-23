6cdd4554cb280dbfeba6d89591efa8e87ea74a2c
NEJ <123035106+NEJulnes@users.noreply.github.com>
2023-01-22 16:50:23 -0800

    Create componets.ts

---
+++ componets.ts
@@ -0,0 +1,22 @@
+// extends Object for cleaning up eslint errors for override toString()
+class ComponentVector extends Object {
+    public readonly dimensions: number[];
+    public constructor(dimensions) {
+        super();
+        this.dimensions = dimensions;
+    }
+    public override toString() {
+        return '[ ' + this.dimensions.map(dimension => dimension).join(', ') + ' ]';
+    }
+}
+
+class ComponentMatrix extends Object {
+    public readonly rows: number[][];
+    public constructor(rows: number[][]) {
+        super();
+        this.rows = rows;
+    }
+    public override toString() {
+        return '[ ' + this.rows.map(dimensions => '[ ' + dimensions.map(dimension => dimension).join(', ') + ' ]').join(', ') + ' ]';
+    }
+}
